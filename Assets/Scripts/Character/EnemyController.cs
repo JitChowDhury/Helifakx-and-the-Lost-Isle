38182662944bb6cf2ba626e6348fbdaf01130b41
JitChowDhury <jitchowdhury8717@gmail.com>
2025-04-16 14:47:16 +0530

    (WIP) updated enemy logic

--- Assets/Scripts/Character/EnemyController.cs
+++ Assets/Scripts/Character/EnemyController.cs
@@ -1,3 +1,5 @@
+using System;
+using RPG.Utility;
 using UnityEngine;
 
 namespace RPG.Character
@@ -5,8 +7,34 @@ namespace RPG.Character
 
     public class EnemyController : MonoBehaviour
     {
+        private GameObject player;
         public float chaseRange = 2.5f;
         public float attackRange = 0.75f;
+
+        [NonSerialized] public float distanceFromPlayer;
+        void Awake()
+        {
+            player = GameObject.FindWithTag(Constants.PLAYER_TAG);
+        }
+
+
+        void Update()
+        {
+            CalculateDistanceToPlayer();
+        }
+
+        private void CalculateDistanceToPlayer()
+        {
+            if (player == null) return;
+
+            Vector3 enemyPosition = transform.position;
+            Vector3 playerPosition = player.transform.position;
+
+            distanceFromPlayer = Vector3.Distance(enemyPosition, playerPosition);
+        }
+
+
+
 
     }
 }
