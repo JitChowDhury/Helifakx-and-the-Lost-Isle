34ac613a2617f0d3c2c8d376c06c1513cac00c8a
JitChowDhury <jitchowdhury8717@gmail.com>
2025-04-19 11:45:53 +0530

    Finished player movement animation

--- Assets/Scripts/Utilities/Constants.cs
+++ Assets/Scripts/Utilities/Constants.cs
@@ -3,5 +3,7 @@ namespace RPG.Utility
     public static class Constants
     {
         public const string PLAYER_TAG = "Player";
+        public const string IS_SHAKING_ANIMATOR_PARAM = "isShaking";
+        public const string SPEED_ANIMATOR_PARAM = "speed";
     }
 }
