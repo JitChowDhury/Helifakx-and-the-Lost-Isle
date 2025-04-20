b0b6cae3ecc3b8dc168b72576516e193dba1f505
JitChowDhury <jitchowdhury8717@gmail.com>
2025-04-20 20:20:50 +0530

    Implemented Player Defeat

--- Assets/Scripts/Character/Health.cs
+++ Assets/Scripts/Character/Health.cs
@@ -1,4 +1,6 @@
 using System;
+using RPG.Utility;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 namespace RPG.Character
 {
@@ -6,10 +8,32 @@ namespace RPG.Character
     {
 
         [NonSerialized] public float healthPoints = 0f;
+        private bool isDefeated = false;
+
+        private Animator animatorCmp;
+
+        void Awake()
+        {
+            animatorCmp = GetComponentInChildren<Animator>();
+        }
         public void takeDamage(float damageAmount)
         {
             healthPoints = Mathf.Max(healthPoints - damageAmount, 0);
-            print(healthPoints);
+            if (healthPoints == 0)
+            {
+                Defeated();
+
+            }
+        }
+
+        private void Defeated()
+        {
+            if (isDefeated) return;
+            isDefeated = true;
+            animatorCmp.SetTrigger(Constants.DEFEATED_ANIMATOR_PARAM);
         }
     }
+
+
+
 }
