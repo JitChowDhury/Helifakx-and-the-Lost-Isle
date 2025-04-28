ba7fbe2a0df5c92aec6bcb202eef318c857734fb
JitChowDhury <jitchowdhury8717@gmail.com>
2025-04-28 12:21:12 +0530

    Add reward scriptable object

---
+++ Assets/Scripts/Quests/RewardSO.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace RPG.Quest
+{
+
+    public class RewardSO : ScriptableObject
+    {
+        public float bonusHealth = 0f;
+        public float bondusDamage = 0f;
+        public int bonusPotions = 0;
+        public bool forceWeaponSwap = false;
+
+        
+    }
+}
