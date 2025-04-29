90c53de8e8712c28d5c8ba1d3c7196f75385893d
JitChowDhury <jitchowdhury8717@gmail.com>
2025-04-29 12:10:37 +0530

    Teleport to next scene

---
+++ Assets/Scripts/Core/Portal.cs
@@ -0,0 +1,23 @@
+using RPG.Utility;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+namespace RPG.Core
+{
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] int nextSceneIndex;
+        private Collider colliderCmp;
+        void Awake()
+        {
+            colliderCmp = GetComponent<Collider>();
+        }
+        void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag(Constants.PLAYER_TAG)) return;
+
+
+            colliderCmp.enabled = false;
+            SceneTransition.Initiate(nextSceneIndex);
+        }
+    }
+}
