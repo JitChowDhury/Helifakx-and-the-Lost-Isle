7b458fcd60e864f04331a13c6409ab1437892136
JitChowDhury <jitchowdhury8717@gmail.com>
2025-05-01 12:16:17 +0530

    Stop player movement in cutscene

--- Assets/Scripts/Core/CinematicController.cs
+++ Assets/Scripts/Core/CinematicController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.Playables;
 using RPG.Utility;
+using Unity.VisualScripting;
 
 namespace RPG.Core
 {
@@ -13,12 +14,39 @@ namespace RPG.Core
             playableDirectorCmp = GetComponent<PlayableDirector>();
             colliderCmp = GetComponent<Collider>();
         }
+        void Start()
+        {
+            colliderCmp.enabled = !PlayerPrefs.HasKey("SceneIndex");
+        }
+
+        void OnEnable()
+        {
+            playableDirectorCmp.played += HandlePlayed;
+            playableDirectorCmp.stopped += HandleStopped;
+        }
+
+
+        void OnDisable()
+        {
+            playableDirectorCmp.played -= HandlePlayed;
+            playableDirectorCmp.stopped -= HandleStopped;
+        }
         void OnTriggerEnter(Collider other)
         {
             if (!other.CompareTag(Constants.PLAYER_TAG)) return;
             playableDirectorCmp.Play();
             colliderCmp.enabled = false;
 
+        }
+
+        private void HandlePlayed(PlayableDirector pd)
+        {
+            EventManager.RaiseCutSceneUpdated(false);
+        }
+
+        private void HandleStopped(PlayableDirector pd)
+        {
+            EventManager.RaiseCutSceneUpdated(true);
         }
     }
 
